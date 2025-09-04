7a9853333e26189dc51178033b190256c53413e3
Narkilla Alankrith <149650035+alankrith123@users.noreply.github.com>
2025-09-04 21:03:00 +0530

    Delete middleware-project/middleware--project directory

--- middleware-project/middleware--project/controllers/bapController.js
+++
@@ -1,8 +0,0 @@
-const { processBAP } = require("../services/bapService");
-
-exports.handleBAP = (req, res) => {
-  let { sessionId, digit } = req.body;
-  digit = String(digit).trim();
-  const message = processBAP(digit);
-  res.json({ sessionId, message });
-};
