49d351bb138b9b8588d25c843dd0e75dde7725b4
Narkilla Alankrith <149650035+alankrith123@users.noreply.github.com>
2025-09-04 21:01:54 +0530

    Delete middle directory

--- middle/middleware/controllers/acsController.js
+++
@@ -1,8 +0,0 @@
-const { processACS } = require("../services/acsService");
-
-exports.handleACS = (req, res) => {
-  let { sessionId, digit } = req.body;
-  digit = String(digit).trim();
-  const message = processACS(digit);
-  res.json({ sessionId, message });
-};
--- middle/middleware/services/acsService.js
+++
@@ -1,5 +0,0 @@
-exports.processACS = (digit) => {
-  if (digit === "1") return "ACS: Your account balance is $500.";
-  if (digit === "2") return "ACS: Recharge processed successfully.";
-  return "ACS: Unknown request.";
-};
