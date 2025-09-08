88377657a66e3eae6d99e3fe7047c6f2c312e904
Narkilla Alankrith <149650035+alankrith123@users.noreply.github.com>
2025-09-08 19:50:07 +0530

    Delete routes directory

--- routes/bapRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { handleBAP } = require("../controllers/bapController");
-
-const router = express.Router();
-router.post("/process", handleBAP);
-
-module.exports = router;
