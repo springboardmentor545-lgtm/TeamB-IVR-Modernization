d242e6e104f59a27208228425109a6046f226cc9
ramyainavolu <153355594+ramyainavolu@users.noreply.github.com>
2025-10-11 15:30:32 +0530

    Delete middleware-project directory

--- middleware-project/services/bapService.js
+++
@@ -1,6 +0,0 @@
-exports.processBAP = (digit) => {
-  if (digit === "3") {
-    return "Connecting you to a live agent. Please hold while we transfer your call. Your estimated wait time is 2 minutes.";
-  }
-  return "BAP: Unknown request.";
-};
